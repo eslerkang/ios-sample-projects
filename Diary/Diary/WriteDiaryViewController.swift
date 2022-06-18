3263ec17a911b4499ed5352b2e9b1dd7a1417154
eslerkang <eslerkang@gmail.com>
2022-06-18 10:48:33 +0900

    feat: add WriteDiaryViewController

---
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 강태준 on 2022/06/18.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
