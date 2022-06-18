7c283314f1d32f912d87d999c4dc5039944cfa08
eslerkang <eslerkang@gmail.com>
2022-06-18 10:12:58 +0900

    diary app initial setting feat: add default sketch board layout using tab view controller, navigation view controller, collectionview

---
+++ Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 강태준 on 2022/06/18.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
---
+++ Diary/Diary/StarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StarViewController.swift
+//  Diary
+//
+//  Created by 강태준 on 2022/06/18.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
