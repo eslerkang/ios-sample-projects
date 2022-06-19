0215219cbecec3cecba445ffd56089ef4ca75ab0
eslerkang <eslerkang@gmail.com>
2022-06-19 19:51:15 +0900

    pomodoro - feat: add date picker, timer info view toggle

--- pomodoro/pomodoro/ViewController.swift
+++ pomodoro/pomodoro/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+
+    var duration = 60
+    var timerStatus: TimerStatus = .end
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureToggleButton()
+    }
+    
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progressView.isHidden = isHidden
+    }
+    
+    func configureToggleButton() {
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
     }
 
-
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch self.timerStatus {
+        case .pause, .start:
+            self.timerStatus = .end
+            self.cancelButton.isEnabled = false
+            self.setTimerInfoViewVisible(isHidden: true)
+            self.datePicker.isHidden = false
+            self.toggleButton.isSelected = false
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+        switch self.timerStatus {
+        case .end:
+            self.timerStatus = .start
+            self.setTimerInfoViewVisible(isHidden: false)
+            self.datePicker.isHidden = true
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+        case .start:
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false
+        case .pause:
+            self.timerStatus = .start
+            self.toggleButton.isSelected = true
+        }
+    }
 }
 
