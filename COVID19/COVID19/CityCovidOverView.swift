dd27e8b4675d67c8e67976bf3daa418f36b91e83
eslerkang <eslerkang@gmail.com>
2022-06-22 21:15:25 +0900

    COVID19 - feat: connect to Corona-19-API to get covid info, make piechart with Charts

---
+++ COVID19/COVID19/CityCovidOverView.swift
@@ -0,0 +1,40 @@
+//
+//  CityCovidOverView.swift
+//  COVID19
+//
+//  Created by 강태준 on 2022/06/22.
+//
+
+import Foundation
+
+struct CityCovidOverview: Codable {
+    let korea: CovidOverview
+    let seoul: CovidOverview
+    let busan: CovidOverview
+    let daegu: CovidOverview
+    let incheon: CovidOverview
+    let gwangju: CovidOverview
+    let daejeon: CovidOverview
+    let ulsan: CovidOverview
+    let sejong: CovidOverview
+    let gyeonggi: CovidOverview
+    let gangwon: CovidOverview
+    let chungbuk: CovidOverview
+    let chungnam: CovidOverview
+    let jeonbuk: CovidOverview
+    let jeonnam: CovidOverview
+    let gyeongbuk: CovidOverview
+    let gyeongnam: CovidOverview
+    let jeju: CovidOverview
+}
+
+struct CovidOverview: Codable {
+    let countryName: String
+    let newCase: String
+    let totalCase: String
+    let recovered: String
+    let death: String
+    let percentage: String
+    let newCcase: String
+    let newFcase: String
+}
--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+
 import Charts
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var totalCaseLabel: UILabel!
@@ -15,6 +17,92 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case let .success(result):
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configurePieChartView(covidOverviewList: covidOverviewList)
+            case let .failure(error):
+                debugPrint(error)
+            }
+        })
+    }
+    
+    func configureStackView(koreaCovidOverview: CovidOverview) {
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
+    }
+    
+    func makeCovidOverviewList(
+        cityCovidOverview: CityCovidOverview
+    ) -> [CovidOverview] {
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.daegu,
+            cityCovidOverview.incheon,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.sejong,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.jeju
+        ]
+    }
+    
+    func configurePieChartView(covidOverviewList: [CovidOverview]) {
+        let entries = covidOverviewList.compactMap { [weak self] overView -> PieChartDataEntry? in
+            guard let self = self else {return nil}
+            return PieChartDataEntry(value: self.removeFormatString(string: overView.newCase), label: overView.countryName, data: overView)
+        }
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        dataSet.colors = ChartColorTemplates.vordiplom() + ChartColorTemplates.joyful() + ChartColorTemplates.liberty() + ChartColorTemplates.pastel() + ChartColorTemplates.material()
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+        self.pieChartView.spin(duration: 0.3, fromAngle: self.pieChartView.rotationAngle, toAngle: self.pieChartView.rotationAngle + 80)
+        self.pieChartView.rotationEnabled = false
+    }
+    
+    func removeFormatString(string: String) -> Double {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    func fetchCovidOverview(completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void) {
+        let url = "https://api.corona-19.kr/korea/country/new"
+        let serviceKey = (Bundle.main.infoDictionary?["API_KEY"]) ?? ""
+        let param = [
+            "serviceKey": serviceKey
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
     }
 }
 
