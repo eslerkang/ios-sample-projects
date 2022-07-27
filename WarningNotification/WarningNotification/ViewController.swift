156a20d4c1855444fe37eebc29d280c68e76fcfb
eslerkang <eslerkang@gmail.com>
2022-07-27 16:26:38 +0900

    WarningNotification - feat: initial setting / connect to Firebase

---
+++ WarningNotification/WarningNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarningNotification
+//
+//  Created by 강태준 on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
