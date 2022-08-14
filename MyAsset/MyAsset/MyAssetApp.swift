6ef361b5ea68a9c9c7d16967defab433dd969f3a
eslerkang <eslerkang@gmail.com>
2022-08-14 22:02:31 +0900

    MyAsset - feat: Initial setting

---
+++ MyAsset/MyAsset/MyAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetApp.swift
+//  MyAsset
+//
+//  Created by 강태준 on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
