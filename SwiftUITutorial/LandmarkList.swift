c1968618510341ace1c1bb573c3305192fe24c69
eslerkang <eslerkang@gmail.com>
2022-08-15 23:32:49 +0900

    feat: create default UI of landmarkList

---
+++ SwiftUITutorial/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  SwiftUITutorial
+//
+//  Created by 강태준 on 2022/08/15.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
