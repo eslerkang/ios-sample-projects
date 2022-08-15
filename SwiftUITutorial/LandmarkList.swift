cf0eb40aa8eb1ebf95cfd7af043b676404ebbf7b
eslerkang <eslerkang@gmail.com>
2022-08-15 23:55:33 +0900

    test: add more devices at LandmarkList Preview

--- SwiftUITutorial/LandmarkList.swift
+++ SwiftUITutorial/LandmarkList.swift
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 13", "iPhone 8 Plus"], id: \.self) {deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
