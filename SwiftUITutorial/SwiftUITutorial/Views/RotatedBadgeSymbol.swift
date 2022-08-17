3adae225361fa7d74af19189a27e986cb622f38b
eslerkang <eslerkang@gmail.com>
2022-08-17 16:08:55 +0900

    feat: draw the badge symbol

---
+++ SwiftUITutorial/SwiftUITutorial/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITutorial
+//
+//  Created by 강태준 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
