22b6a6f914d28d218457d54afa8c4005950830ae
eslerkang <eslerkang@gmail.com>
2022-08-22 23:50:11 +0900

    MyAsset - feat: create CreditCardAmount column to Asset

--- MyAsset/MyAsset/Entity/Asset.swift
+++ MyAsset/MyAsset/Entity/Asset.swift
@@ -25,10 +25,66 @@ class AssetData: Identifiable, ObservableObject, Codable {
     let id: Int
     let title: String
     let amount: String
+    let creditCardAmounts: [CreditCardAmount]?
     
-    init(id: Int, title: String, amount: String) {
+    init(id: Int, title: String, amount: String, creditCardAmounts: [CreditCardAmount]? = nil) {
         self.id = id
         self.title = title
         self.amount = amount
+        self.creditCardAmounts = creditCardAmounts
     }
+}
+
+
+enum CreditCardAmount: Identifiable, Codable {
+    case previousMonth(amount: String)
+    case currentMonth(amount: String)
+    case nextMonth(amount: String)
+    
+    var id: Int {
+        switch self {
+        case .previousMonth:
+            return 0
+        case .currentMonth:
+            return 1
+        case .nextMonth:
+            return 2
+        }
+    }
+    
+    var amount: String {
+        switch self {
+        case .previousMonth(let amount),
+                .currentMonth(let amount),
+                .nextMonth(let amount):
+            return amount
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case previousMonth
+        case currentMonth
+        case nextMonth
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let value = try? values.decode(String.self, forKey: .previousMonth) {
+            self = .previousMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .currentMonth) {
+            self = .currentMonth(amount: value)
+            return
+        }
+        
+        if let value = try? values.decode(String.self, forKey: .nextMonth) {
+            self = .nextMonth(amount: value)
+            return
+        }
+        
+        throw fatalError("ERROR: CreditCardAmounts JSON Decoding")
+     }
 }
