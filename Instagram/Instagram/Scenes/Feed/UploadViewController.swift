4f0f3adea8fbcd0904a0de865f6eb21c0550bdd5
eslerkang <eslerkang@gmail.com>
2022-08-30 22:12:36 +0900

    Instagram - feat: connect user's image to imageView of UploadViewController / create textView to get user's text

--- Instagram/Instagram/Scenes/Feed/UploadViewController.swift
+++ Instagram/Instagram/Scenes/Feed/UploadViewController.swift
@@ -20,8 +20,11 @@ final class UploadViewController: UIViewController {
         
         return imageView
     }()
-    private lazy var textView: UITextView = {
+    private lazy var textField: UITextView = {
         let textView = UITextView()
+        textView.text = "문구를 입력하세요"
+        textView.delegate = self
+        textView.textColor = .secondaryLabel
         textView.font = .systemFont(ofSize: 15)
         
         return textView
@@ -44,6 +47,30 @@ final class UploadViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
+
+extension UploadViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard textView.textColor == .secondaryLabel
+        else {
+            return
+        }
+        
+        textView.text = nil
+        textView.textColor = .label
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.count == 0 {
+            textView.textColor = .secondaryLabel
+            textView.text = "문구를 입력하세요"
+        }
     }
 }
 
@@ -73,7 +100,7 @@ private extension UploadViewController {
     func setupLayout() {
         [
             imageView,
-            textView
+            textField
         ].forEach {
             view.addSubview($0)
         }
@@ -82,8 +109,15 @@ private extension UploadViewController {
         
         imageView.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(imageViewInset)
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(imageViewInset)
-            $0.width.height.equalTo(120)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.width.height.equalTo(100)
+        }
+        
+        textField.snp.makeConstraints {
+            $0.leading.equalTo(imageView.snp.trailing)
+            $0.trailing.equalToSuperview().inset(imageViewInset)
+            $0.top.equalTo(imageView)
+            $0.bottom.equalTo(imageView)
         }
     }
     
